e59d7dc29847d8284366f2ac425794f3ce0a78ec
JonathanXu4 <59599239+JonathanXu4@users.noreply.github.com>
2023-12-11 13:49:05 -0800

    updated password

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://ninja:yc2HVkzdzTYeUeAV@cluster0.pkknrum.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://ninja:93FF6W2BppfCDd0K@cluster0.pkknrum.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
-    .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        console.log('DB Connected!');
-    })
-    .catch(error => {
-        console.log('Connection Error: ${err.message}');
-    });
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB Connected!");
+  })
+  .catch((error) => {
+    console.log("Connection Error: ${err.message}");
+  });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB Connection Error'));
+db.on("error", console.error.bind(console, "MongoDB Connection Error"));
 
 module.exports = db;
